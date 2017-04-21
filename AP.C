438ad754f190d38bd40e1b6f754e9a95773e3850
Iván Cárdenas <icardena@eafit.edu.co>
2017-04-20 23:43:49 -0500

    .

--- AP.C
+++ AP.C
@@ -14,67 +14,27 @@ unsigned long timeoutPeriod = 3000;
 
 const uint64_t pipes[2] = { 0x7878787878LL, 0xB3B4B5B6F1LL };
 
-unsigned long data = 10101010;
 
-/*void showData(void) {
-	printf("Data: ");
-	for(int i = 0; i < 32; i++) {
-		if (isprint(data[i])) printf("%c", data[i]);
-		else printf(".");
-	}
-	printf("\n\r");
-}*/
 
 int main(int argc, char** argv) {
-	/*const int role_rx = 0, role_tx = 1;
-	int role = role_rx;*/
-
-	/*printf("\n ************ Role Setup ***********\n");
-	string input = "";
-	char myChar = {0};*/
-
-	/*cout << "Choose a role: Enter 0 for Rx, 1 for Tx (CTRL+C to exit) \n>";
-	getline(cin,input);*/
-
-	/*if (input.length() == 1) {
-		myChar = input[0];
-		if (myChar == '0') {
-			cout << "Role: Rx " << endl << endl;
-		} else {
-			cout << "Role: Tx " << endl << endl;
-		        role = role_tx;
-	        }
-	}*/
-
-	/*switch(role) {
-		case role_rx :
-			radioNumber = 0;
-			break;
-		case role_tx :
-			radioNumber=1;
-			 break;								}*/
 
 	radio.begin();
 	radio.setChannel(110);
 	radio.setPALevel(RF24_PA_MIN);
 	radio.setDataRate(RF24_250KBPS);
 
-	/*if (!radioNumber) {
-		radio.openWritingPipe(pipes[0]);
-		radio.openReadingPipe(1,pipes[1]);
-		memset(&data,'\0',sizeof(data));
-		radio.startListening();
-	} else {*/
-								/*}*/
+	radio.openWritingPipe(pipes[1]);
+	radio.openReadingPipe(1, pipes[0]);
 
 	radio.printDetails();
 
-
 	while (1) {
-		radio.openWritingPipe(pipes[1]);
+
+		unsigned long data = 10101010;
+
 		radio.stopListening();
 		//if (radioNumber) {
-		auto init_time = chrono::high_resolution_clock::now();
+		// auto init_time = chrono::high_resolution_clock::now();
 
 		/* if (!radio.write(&data, sizeof(unsigned long)))
 			printf("Failed, could not send.");
@@ -90,8 +50,7 @@ int main(int argc, char** argv) {
 			fflush(stdout);
 		// } else {*/
 
-		radio.openReadingPipe(1, pipes[0]);
-		//memset(&data, '\0', sizeof(data)); // ?
+
 		radio.startListening();
 
 		// finalTime
@@ -110,9 +69,10 @@ int main(int argc, char** argv) {
 
 		if (radio.available()) {
 				//auto init_time_r = chrono::high_resolution_clock::now();
-				radio.read(&data, sizeof(unsigned long));
+				unsigned long init_time_r;
+				radio.read(&init_time_r, sizeof(unsigned long));
 				//auto end_time = chrono::high_resolution_clock::now();
-
+				printf(init_time_r);
 				//printf("Elapsed time: ");
 				//cout << chrono::duration_cast<chrono::nanoseconds>(end_time - init_time).count() << endl;
 				//printf(" nanoseconds");
