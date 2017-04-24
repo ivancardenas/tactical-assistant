8bd0d3eeac62f99b5b0d10ffbfb5f53944583457
Iván Cárdenas <icardena@eafit.edu.co>
2017-04-24 00:31:49 -0500

    Update time.

--- AP.C
+++ AP.C
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
 
 		radio.startListening();
 
-		while (!radio.available() && timeout < 20) timeout++;
+		while (!radio.available() && timeout < 100) timeout++;
 
 		radio.read(&rtime, sizeof(unsigned long long));
 
