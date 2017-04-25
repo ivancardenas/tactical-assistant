d9fed7fdc09617eeab9afe17b577b924e27ca87e
Iván Cárdenas <icardena@eafit.edu.co>
2017-04-24 21:34:42 -0500

    Update time.

--- AP.C
+++ AP.C
@@ -43,6 +43,7 @@ int main(int argc, char** argv) {
 			<nanoseconds>(start.time_since_epoch()).count() - 1493080000000000000;
 
 		printf("%llu\n", stn);
+		stn=999999999999;
 
 		if (!radio.write(&stn, sizeof(uint64_t))) {}
 
