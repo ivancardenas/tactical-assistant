afc9eb0d2adb7f73d4839ce35b04074497816d4b
Iván Cárdenas <icardena@eafit.edu.co>
2017-04-24 20:36:42 -0500

    Update time.

--- AP.C
+++ AP.C
@@ -40,7 +40,7 @@ int main(int argc, char** argv) {
 
 		auto start = high_resolution_clock::now();
 		unsigned long long stn = duration_cast
-			<nanoseconds>(start.time_since_epoch()).count() - 1493014497000000000;
+			<nanoseconds>(start.time_since_epoch()).count() /* - 1493014497000000000*/;
 
 		printf(" Mande: %llu\n", stn);
 
@@ -56,7 +56,7 @@ int main(int argc, char** argv) {
 
 		auto ended = high_resolution_clock::now();
 		unsigned long long etn = duration_cast
-			<nanoseconds>(start.time_since_epoch()).count() - 1493014497000000000;
+			<nanoseconds>(start.time_since_epoch()).count() /* - 1493014497000000000*/;
 
 		unsigned long long res = etn - stn;
 		printf("Received: %llu \n", etn);
